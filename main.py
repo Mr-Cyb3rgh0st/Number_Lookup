48fb45be43acc46fa149491722bd5cd1c697b876
Mr. Cyb3rgh0st <97544400+Mr-Cyb3rgh0st@users.noreply.github.com>
2024-10-31 12:19:29 +0600

    eyecon  api update

--- main.py
+++ main.py
@@ -14,24 +14,36 @@ def logo():
     
 def main():
     number = (input(Fore.GREEN+"[*] Enter a Number: "))
+
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
-        "accept": "application/json",
-        "e-auth-v": "e1",
-        "e-auth": "345eb9cc-3c68-415e-82ae-f41882255d01",                   
-        "e-auth-c": "33",
-        "e-auth-k": "PgdtSBeR0MumR7fO",
-        "accept-charset": "UTF-8",
-        "content-type": "application/x-www-form-urlencoded; charset=utf-8",
-        "Host": "api.eyecon-app.com",
-        "Connection": "Keep-Alive",
-        "Accept-Encoding": "gzip"
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+        'Connection': 'Keep-Alive',
+        'Accept': 'application/json',
+        'e-auth-v': 'e1',
+        'e-auth': 'dd4190a9-e11e-453d-86ea-2246ddfbd393',
+        'e-auth-c': '41',
+        'e-auth-k': 'PgdtSBeR0MumR7fO',
+        'accept-charset': 'UTF-8',
+        'content-type': 'application/x-www-form-urlencoded; charset=utf-8',
     }
 
-    url = f"https://api.eyecon-app.com/app/getnames.jsp?cli=88{number}&lang=en&is_callerid=true&is_ic=true&cv=vc_510_vn_4.0.510_a&requestApi=okHttp&source=MenifaFragment"
-    response = requests.get(url, headers=headers)
+    params = {
+        'cli': f'88{number}',
+        'lang': 'en',
+        'is_callerid': 'true',
+        'is_ic': 'true',
+        'cv': 'vc_530_vn_4.0.530_a',
+        'requestApi': 'URLconnection',
+        'source': 'MenifaFragment',
+    }
 
-    print(Fore.GREEN+"[+] Name:", response.content.decode('utf-8').split('"name":"')[1].split('","')[0])
+    response = requests.get('https://api.eyecon-app.com/app/getnames.jsp', params=params, headers=headers)
+    if response.status_code == 200:
+        res_data= response.json()
+        print(Fore.GREEN+"[+] Name:", res_data[0]['name'])
+    else:
+        print(Fore.RED+"[-] Error: ", response.status_code)
+    
 
 logo()
 main()
